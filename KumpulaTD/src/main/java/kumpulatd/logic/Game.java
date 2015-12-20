ab93a42859a31d109bc242208e5c40620c1b3cea
kummitus <antti.hakli@helsinki.fi>
2015-12-20 15:21:55 +0200

    Practicing github

--- KumpulaTD/src/main/java/kumpulatd/logic/Game.java
+++ KumpulaTD/src/main/java/kumpulatd/logic/Game.java
@@ -12,7 +12,7 @@ package kumpulatd.logic;
 public class Game {
 
     public void init() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
     }
     
 }
