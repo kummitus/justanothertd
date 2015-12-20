59095dc91bf2b5bef4ba0edebf8000c9808956fe
kummitus <antti.hakli@helsinki.fi>
2015-12-20 17:02:41 +0200

    Retry

---
+++ KumpulaTD/src/test/java/kumpula/logic/FuksiTest.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kumpula.logic;
+
+import kumpulatd.logic.Fuksi;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author antti
+ */
+public class FuksiTest {
+    
+    Fuksi fuksi;
+    
+    public FuksiTest() {
+    }
+    
+
+    
+    @Before
+    public void setUp() {
+        
+        fuksi = new Fuksi();
+    }
+    
+    @Test
+    public void testNewFuksi(){
+        assertEquals(100, fuksi.getHP());
+        assertEquals(5, fuksi.getSpeed());
+    }
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
