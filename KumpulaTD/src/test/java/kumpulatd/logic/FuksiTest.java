4c378cfc1a3a8d8489ff0fdeeb34cf5267da2962
kummitus <antti.hakli@helsinki.fi>
2015-12-20 17:23:05 +0200

    Retry

--- KumpulaTD/src/test/java/kumpulatd/logic/FuksiTest.java
+++ KumpulaTD/src/test/java/kumpulatd/logic/FuksiTest.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package kumpula.logic;
+package kumpulatd.logic;
 
 import kumpulatd.logic.Fuksi;
 import org.junit.After;
--- KumpulaTD/src/test/java/kumpulatd/ui/MenuTest.java
+++ KumpulaTD/src/test/java/kumpulatd/ui/MenuTest.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package kumpula.ui;
+package kumpulatd.ui;
 
 import kumpulatd.ui.Menu;
 import org.junit.After;
