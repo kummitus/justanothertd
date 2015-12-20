a0205ef095539f454ac8362c2f616e4390e2dedd
kummitus <antti.hakli@helsinki.fi>
2015-12-20 15:04:58 +0200

    Insert more classes

---
+++ KumpulaTD/src/main/java/kumpulatd/ui/Menu.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kumpulatd.ui;
+
+import java.util.Scanner;
+import kumpulatd.logic.Game;
+
+/**
+ *
+ * @author antti
+ */
+public class Menu {
+
+    private Scanner reader;
+
+    public Menu() {
+        reader = new Scanner(System.in);
+
+        menuLoop();
+    }
+
+    private void menuLoop() {
+        System.out.println("Select:");
+        while (true) {
+            System.out.println("  1: New Game"
+                    + "\n  2: Quit");
+            String choice = reader.nextLine();
+            if(choice.equals("1")){
+                new Game().init();
+            } else if(choice.equals("2")){
+                break;
+            }
+        }
+    }
+}
